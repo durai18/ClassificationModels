357b37966ba0ef5c10fb6dd7762b6d27920927c0
durai18 <93244154+durai18@users.noreply.github.com>
2026-02-15 18:27:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import pandas as pd
 st.title("Wine Quality Classification")
 
 # Load trained model
-#with open("model/trained_model.pkl", "rb") as f:
-with open("model/knn.pkl", "rb") as f:
+with open("model/trained_model.pkl", "rb") as f:
+#with open("model/knn.pkl", "rb") as f:
     model = pickle.load(f)
 
 # User input
@@ -21,4 +21,5 @@ if st.button("Predict Quality"):
                             columns=["fixed acidity", "volatile acidity", "citric acid", "residual sugar", "alcohol"])
     prediction = model.predict(features)
     st.success(f"Predicted Wine Quality: {prediction[0]}")
+
 
